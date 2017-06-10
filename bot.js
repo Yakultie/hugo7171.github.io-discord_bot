8ffa09d6451c8068d4e2695e4f2c99eaf3b8aea8
hugo7171 <hugowong71@gmail.com>
2017-06-10 23:31:08 +0800

    reveal

--- bot.js
+++ bot.js
@@ -1,12 +1,42 @@
-// Requires
-const Discord = require('discord.js')
 
-// Create an instance of a Discord client
-const client = new Discord.Client()
+console.log("back off hackerz")
+const Discord = require('discord.js');
+const bot = new Discord.Client();
 
-// The bot is ready
-client.on('ready', () => {
-  console.log('Bot Started')
+// Basic Code
+
+//functions
+
+
+//end of functions
+bot.on('message',(message)=>{
+  var currentChannel = message.channel
+    if(message.content=="start"){
+      currentChannel.sendMessage("https://sd.keepcalm-o-matic.co.uk/i/sit-back-relax-and-enjoy--7.png")
+        var loop = setInterval(backSide, 15000);
+        function backSide() {
+          currentChannel.sendMessage("*rule34 overwatch")
+        }
+    }
+    if(message.content=="burn"){
+      message.channel.sendMessage("https://m.popkey.co/2e18f2/VlgXG.gif")
+    }
+    if (message.content=="truth or dare") {
+      random = Math.random()
+      if (random>0.5) {
+        currentChannel.sendMessage("Truth")
+      }else{
+        currentChannel.sendMessage("Dare")
+      }
+      }
+      if(message.content=="spam"){
+        for (var i = 0; i < 20; i++) {
+          currentChannel.sendMessage("Ignore")
+        }
+    }
+    if (message.content =="teo is a retard") {
+      clearInterval(loop);
+    }
 })
 
 client.login(process.env.BOT_TOKEN)
